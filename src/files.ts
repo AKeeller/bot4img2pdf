a8bfae771a4fada076d6a14775dfe12782776e79
Alberto Amoruso <alberto.am@me.com>
2021-04-18 21:31:02 +0200

    New renameFile function

--- src/files.ts
+++ src/files.ts
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import path from 'path'
 
 export module Files {
 
@@ -17,6 +18,14 @@ export module Files {
 
 	export function isEmpty(folder: string) {
 		return !fs.existsSync(folder) || (fs.existsSync(folder) && fs.readdirSync(folder).length <= 0)
+	}
+
+	export function renameFile(filePath: string, newName: string, keepExtension = false) {
+		const dirname = path.dirname(filePath)
+		const extension = path.extname(filePath)
+		const newFilePath = dirname + '/' + newName + (keepExtension ? extension : '')
+
+		fs.rename(filePath, newFilePath, (err) => { if (err) console.error(err) })
 	}
 
 }
