d8d4faa2e1165c90f34e8e9c3a39d64051fd7367
Alberto Amoruso <alberto.am@me.com>
2023-03-08 23:59:26 +0100

    feat(bot): use factories to create `options`

--- src/bot.ts
+++ src/bot.ts
@@ -1,18 +1,24 @@
 import TelegramBot from "node-telegram-bot-api"
+import PollingOptionsFactory from "./options-factory/polling-options-factory"
+import WebhookOptionsFactory from "./options-factory/webhook-options-factory"
 
 const token = process.env.TOKEN
 
 if (!token)
 	throw new Error("Token not found. Create a .env file an put your token there.")
 
-let options: TelegramBot.ConstructorOptions
-
-if (process.env.USE_WEBHOOK)
-	options = { webHook: true }
-else if (!process.env.CERT || !process.env.KEY)
-	options = { polling: true }
-else
-	options = { webHook: { cert: process.env.CERT, key: process.env.KEY } }
+const factory = process.env.WEBHOOK_URL ? new WebhookOptionsFactory() : new PollingOptionsFactory()
+const options = factory.createOptions()
 
 const bot = new TelegramBot(token, options)
+
+if (process.env.WEBHOOK_URL) {
+	const url = `${process.env.WEBHOOK_URL}/bot${process.env.TOKEN}`
+	const options = { certificate: process.env.CERT }
+
+	bot.setWebHook(url, options)
+		.then(_ => console.log(`WebHook set to ${process.env.WEBHOOK_URL}`))
+		.catch(reason => setTimeout(() => { throw new Error(`${reason}`) }))
+}
+
 export default bot;
