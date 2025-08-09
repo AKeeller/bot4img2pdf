83ebbfd620009c7afce6dabce74a6488d22cb601
Alberto Amoruso <alberto.am@me.com>
2025-08-09 18:23:01 +0200

    feat(state): `next` can now return a `Promise`

--- src/state/state.ts
+++ src/state/state.ts
@@ -1,5 +1,5 @@
 import TelegramBot from "node-telegram-bot-api";
 
 export default interface State {
-	next(msg: TelegramBot.Message): State | undefined
+	next(msg: TelegramBot.Message): State | undefined | Promise<State | undefined>
 }
